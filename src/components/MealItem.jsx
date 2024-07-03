5129577d2f9aae4a6228c0efe25c9468cd3feaf8
Alan Mong <alanmongx@gmail.com>
2024-07-03 13:44:02 -0700

    add formatter

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,3 +1,5 @@
+import { currentFormatter } from "../util/formatting";
+
 export default function MealItem({ meal }) {
   return (
     <li className="meal-item">
@@ -5,7 +7,7 @@ export default function MealItem({ meal }) {
         <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
-          <p id="meal-item-price">{meal.price}</p>
+          <p id="meal-item-price">{currencyFormatter.format(meal.price)}</p>
           <p id="meal-item-description">{meal.description}</p>
         </div>
         <p id="meal-item-actions">
